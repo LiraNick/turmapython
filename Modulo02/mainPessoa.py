f651791614473ad5fd9ff73f8489e02662017cda
Nicolas Lira Galiano <nicolas.galiano@proway.treina>
2022-10-19 22:00:19 -0300

    upando aulas

---
+++ Modulo02/mainPessoa.py
@@ -0,0 +1,13 @@
+from controller import salvar, listar
+
+def menu():
+    print("="*20, "menu", "="*20)
+
+    conde = "sim"
+    while cond == "sim":
+        nome = salvar
+        var = str(input("deseja continuar \n sim \n não \n :>"))
+
+menu ()
+
+print("A lista de nomes inseridos\n")
