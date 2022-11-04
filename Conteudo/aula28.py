9b19efda6bb5ba11dc17b72620687723a33f1ae1
Nicolas Lira Galiano <nicolas.galiano@proway.treina>
2022-11-03 22:02:12 -0300

    upando aulas

---
+++ Conteudo/aula28.py
@@ -0,0 +1,5 @@
+class Conta:
+
+    def __init__(self):
+        print(f"Imprimindo{self}")
+        
---
+++ Conteudo/aula28main.py
@@ -0,0 +1,6 @@
+from aula28 import Conta
+
+conta = Conta()
+
+print(f"Imprimindo espaco objeto {conta}")
+print(type(conta))
